9f0f5472c6d97c321cdb48cdb2594634d2505175
Zeynep <doguszeynep@gmail.com>
2022-05-15 17:43:03 +0300

    remove unnecessary objects from constant class

--- information-service/src/main/java/com/philips/informationservice/builder/constants/QueryConstants.java
+++ information-service/src/main/java/com/philips/informationservice/builder/constants/QueryConstants.java
@@ -13,29 +13,4 @@ public class QueryConstants {
             "INNER JOIN schedule s ON s.professor_id=p.id " +
             "INNER JOIN course c ON c.id=s.course_id GROUP BY p.name";
 
-    public static String INSERT_INTO_COURSE = "INSERT INTO course (id, name, department_id, credits) values ";
-
-    public static String INSERT_INTO_PROFESSOR = "INSERT INTO professor (id, name, department_id) values ";
-
-    public static String INSERT_INTO_DEPARTMENT = "INSERT INTO department (id, name) values ";
-
-    public static String INSERT_INTO_SCHEDULE = "INSERT INTO schedule (professor_id, course_id, semester, year) values";
-
-    public static String SELECT_COURSE = "SELECT * FROM course WHERE id=";
-
-    public static String SELECT_PROFESSOR = "SELECT * FROM professor WHERE id=";
-
-    public static String SELECT_DEPARTMENT = "SELECT * FROM department WHERE id=";
-
-    public static String SELECT_SCHEDULE = "SELECT * FROM schedule WHERE professor_id = ? and course_id = ?";
-
-    public static String DELETE_COURSE = "DELETE FROM course WHERE id=";
-
-    public static String DELETE_PROFESSOR = "DELETE FROM professor WHERE id=";
-
-    public static String DELETE_DEPARTMENT = "DELETE FROM department WHERE id=";
-
-    public static String DELETE_SCHEDULE = "DELETE FROM schedule WHERE professor_id = ? and course_id = ?";
-
-
 }
